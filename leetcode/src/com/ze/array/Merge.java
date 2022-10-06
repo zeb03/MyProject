3f406ca3615e2674e039b7558a0a9fa9669f70a4
zeb <2829806354@qq.com>
2022-10-06 23:19:29 +0800

    commit

--- leetcode/src/com/ze/array/Merge.java
+++ leetcode/src/com/ze/array/Merge.java
@@ -1,11 +1,31 @@
 package com.ze.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @author ze
  * @Time 2022-10-05 0:00
  */
 public class Merge {
     public int[][] merge(int[][] intervals) {
-        return null;
+        if(intervals.length == 0){
+            return intervals;
+        }
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+        List<int[]> list = new ArrayList<>();
+        int []temp = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            if(temp[1] >= intervals[i][0]){
+                temp[1] = Math.max(temp[1],intervals[i][1]);
+            }else {
+                list.add(temp);
+                temp = intervals[i];
+            }
+        }
+        list.add(temp);
+        return list.toArray(new int[list.size()][2]);
     }
 }
